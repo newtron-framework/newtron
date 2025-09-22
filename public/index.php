9eea205a2338828b5656104afd168f732acba40d
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-22 13:26:22 -0400

    Scaffold basic application (#1)
    * setup composer
    
    * app entry point
    
    * add config

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Newtron\Core\Application\App;
+
+App::create(dirname(__DIR__));
+
+// Register any custom service providers here
+// App::addServiceProvider($provider);
+
+App::run();
