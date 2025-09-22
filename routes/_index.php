0392cc1fbb058efa303a0596d6aa1fea5c91c066
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-22 15:39:32 -0400

    support routing features

---
+++ routes/_index.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+use Newtron\Core\Application\App;
+use Newtron\Core\Http\Response;
+use Newtron\Core\Routing\FileRoute;
+
+class Test extends FileRoute {
+  public function get(): mixed {
+    return [];
+  }
+
+  public function render(mixed $data): mixed {
+    return Response::create(
+      "Path requested: " . App::getRequest()->getPath() . 
+        "\n<p>Route data:</p><pre>" . print_r($data, true) . "</pre>" . 
+        "\n<p>Config:</p><pre>" . print_r(App::getConfig()->all(), true) . "</pre>"
+    );
+  }
+}
+
+return new Test();
