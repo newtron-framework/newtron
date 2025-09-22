a171f034a191ce6c706ae2a9c948235b7bc76355
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-22 16:43:14 -0400

    Support routing features (#2)
    * support routing features
    
    * support dynamic routes

---
+++ routes/test.[name].php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types=1);
+
+use Newtron\Core\Application\App;
+use Newtron\Core\Http\Response;
+use Newtron\Core\Routing\FileRoute;
+
+class Test extends FileRoute {
+  public function get(string $name): array {
+    return [
+      'name' => $name,
+    ];
+  }
+
+  public function render(mixed $data): mixed {
+    return Response::create(
+      "Path requested: " . App::getRequest()->getPath() . 
+        "\n<p>Route data:</p><pre>" . print_r($data, true) . "</pre>" . 
+        "\n<p>Config:</p><pre>" . print_r(App::getConfig()->all(), true) . "</pre>"
+    );
+  }
+}
+
+return new Test();
