a7fafc9c407ac03662ef93d3577cac8028bb8e8c
James <78121730+itsnewtjam@users.noreply.github.com>
2025-09-26 23:14:05 -0400

    update for core 0.1.0

--- config/app.php
+++ config/app.php
@@ -2,5 +2,5 @@
 
 return [
   'name' => 'Newtron',
-  'debug' => false,
+  'debug' => true,
 ];
--- public/index.php
+++ public/index.php
@@ -3,17 +3,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use Newtron\App\Launcher;
 use Newtron\Core\Application\App;
 
 App::create(dirname(__DIR__));
 
-// Register any custom service providers here
-// App::addServiceProvider($provider);
-
-App::setErrorPage(404, 'not-found');
-
-$am = App::getAssetManager();
-$am->registerStylesheet('global', '/style.css');
-$am->useStylesheet('global');
+Launcher::setup();
 
 App::run();
